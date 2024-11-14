5f14bfd29a91bbce39490576882ce8e97001466e
Mikhail Li <leemisha98@gmail.com>
2024-11-15 01:33:31 +0200

    Distortion fixed

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-const density = 'Ñ@#W$9876543210?!abc;:+=-,._ ';
+const density = 'Ñ@#W$9876543210?!abc;:+=-,._    ';
 
 let video;
 let asciiDiv;
@@ -9,28 +9,29 @@ function setup() {
   video.size(120, 60);
   video.hide();
   asciiDiv = createDiv();
+  asciiDiv.class('ascii-container');
 }
 
 function draw() {
   video.loadPixels();
   let asciiImage = '';
-  
+
   for (let j = 0; j < video.height; j++) {
+    let row = '';
     for (let i = video.width - 1; i >= 0; i--) {
       const pixelIndex = (i + j * video.width) * 4;
       const r = video.pixels[pixelIndex];
       const g = video.pixels[pixelIndex + 1];
       const b = video.pixels[pixelIndex + 2];
-      const avg = (r + g + b) / 3; 
+      const avg = (r + g + b) / 3;
 
       const len = density.length;
-      const charIndex = floor(map(avg, 0, 255, len, 0));
-      
+      const charIndex = floor(map(avg, 0, 255, len - 1, 0));
+
       const c = density.charAt(charIndex);
-      if (c == ' ') asciiImage += '&nbsp;';
-      else asciiImage += c;
+      row += (c == ' ') ? '&nbsp;' : c;
     }
-    asciiImage += '<br/>';
+    asciiImage += row + '<br/>';
   }
   asciiDiv.html(asciiImage);
 }
